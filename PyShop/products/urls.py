8f075dae8d37a206e7822f4b30123c327d149397
Gabriel <circiu_gabriel@outlook.com>
2021-11-14 12:58:07 +0100

    New stuff

--- PyShop/products/urls.py
+++ PyShop/products/urls.py
@@ -1,1 +1,10 @@
-urlpatterns =
+from django.urls import path
+from . import views
+
+# /products
+# /products/1/detail
+# /products/new
+urlpatterns = [
+    path('', views.products),
+    path('new/', views.new_products),
+]
